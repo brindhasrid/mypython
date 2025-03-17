45bacaa33674cb99548b9ffae27b000789a02cee
brindhasrid <brindhasrid@gmail.com>
2025-03-17 21:20:41 +0530

    Create getsecindLargest.py

---
+++ getsecindLargest.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getSecondLargest(self, arr):
+        largest = second_largest = None
+
+        for num in arr:
+            if largest is None or num > largest:
+                second_largest = largest
+                largest = num
+            elif num < largest and (second_largest is None or num > second_largest):
+                second_largest = num
+
+        return second_largest if second_largest is not None else -1
